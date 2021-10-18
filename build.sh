bf9ffce023acdc0953e3e65bf602e90c437b3f09
Sudhir Yadav <sudhiryadav.igi@gmail.com>
2021-10-19 03:01:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -70,7 +70,8 @@ rom_seven(){
      repo init --depth=1 --no-repo-verify -u https://github.com/PotatoProject/manifest -b frico-release -g default,-device,-mips,-darwin,-notdefault
      git clone https://github.com/TheSanty/local_manifests.git -b $rom .repo/local_manifests
      repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
-     export ALLOW_MISSING_DEPENDENCIES=true
+     rm -rf hardware/qcom-caf/wlan
+     git clone https://github.com/aex-tmp/platform_hardware_qcom_wlan.git -b 12.x hardware/qcom-caf/wlan
      source build/envsetup.sh && lunch potato_whyred-userdebug
 }
 
