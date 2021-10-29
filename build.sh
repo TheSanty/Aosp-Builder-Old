3503f3b818ef9642a62365570eaecc9c6f8d42de
TheSanty <sudhiryadav.igi@gmail.com>
2021-10-29 12:27:18 +0530

    Build Corvus Official For Whyred
    Signed-off-by: TheSanty <sudhiryadav.igi@gmail.com>
    Change-Id: Ie4484a8d908b72fa3c3d0ab92f016f400d1ea10c

--- build.sh
+++ build.sh
@@ -28,6 +28,9 @@ rom_two(){
      repo init --depth=1 --no-repo-verify -u https://github.com/Corvus-R/android_manifest.git -b 11 -g default,-device,-mips,-darwin,-notdefault
      git clone https://github.com/TheSanty/local_manifests.git -b $rom .repo/local_manifests
      repo sync -c --no-clone-bundle --no-tags --optimized-fetch --force-sync -j$(nproc --all)
+     git clone https://${TOKEN2}@github.com/Corvus-R/.certs certs
+     export RAVEN_LAIR=Official
+     export USE_DEXOPT=true
      . build/envsetup.sh && lunch corvus_whyred-user
 }
 
